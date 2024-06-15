a1fb8279e3a498a23d9cacbbf7708172d337b67a
Teteuumateus <mateusandrade.brasil@gmail.com>
2024-06-15 12:47:59 -0300

    Adicionado lógica do jogo

--- jogo.js
+++ jogo.js
@@ -1,7 +1,12 @@
 // Varíavel de controle para indicar se o jogo está sendo executado/andamento ou parado.
 let executando = false
+let acertos = 0;
+
 const startBotao = document.querySelector("#btn--start");
 const sorteio = document.querySelector(".num--atual");
+
+const placarVit = document.querySelector(".num--vit");
+const placarDer = document.querySelector(".num--der");
 
 // const start = document.getElementById("btn--start")
 
@@ -13,12 +18,42 @@ function startGame() {
         alert("jogo já está em andamento...")
         return
     } else {
-        let numAleatorio = Math.floor(Math.random()*20+1)
-        sorteio.textContent = numAleatorio
+        sorteio.textContent = Math.floor(Math.random()*20+1)
         executando = true
     }
 }
 
 function verificarResposta(palpite) {
-    alert(`O palpite foi ${palpite}`)
+    let numAnterior = Number(sorteio.textContent);
+    sorteio.textContent = Math.floor(Math.random()*20+1)
+    let numNovo = sorteio.textContent;
+    if (palpite === "alto" && numNovo > numAnterior) {
+        acertos++
+        verificarVitoria()
+
+    } else if (palpite ==="baixo" && numNovo < numAnterior) {
+        acertos++
+        verificarVitoria();
+
+    } else {
+        gameOver();   
+    }
+}
+
+function sortearNum() {
+    return sorteio.textContent = Math.floor(Math.random()*20+1)
+}
+
+function verificarVitoria() {
+    if (acertos >=3) {
+        placarVit.textContent++
+        acertos = 0;
+    }
+}
+
+function gameOver() {
+    placarDer.textContent++
+    executando = false;
+    sorteio.textContent = "";
+    acertos = 0
 }
